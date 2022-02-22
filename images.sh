2644ba8299d542a5f38c16541f316bf6f47e2e69
mark <leis17@163.com>
2022-02-22 18:19:08 +0800

    fix images.sh bug

--- images.sh
+++ images.sh
@@ -2,34 +2,51 @@
 
 imagesFileName="images.yaml"
 
-imagesTmpFileName="images.yaml.tmp"
+imagesTmpFileName="images-bak.yaml"
+
+docker ps -q  > $imagesTmpFileName
+
+imageIds=$(cat ./$imagesTmpFileName)
+
+rm -rf $imagesFileName
+touch $imagesFileName
+
+for id in $imageIds
+do
+  echo $id
+  docker inspect --format='{{json .Config.Image}}' $id >> $imagesFileName
+done
 
 touch $imagesTmpFileName
 echo "#!/usr/bin/env bash" > $imagesTmpFileName
 
-rm -rf $imagesFileName
-
-docker image ls --format "{{.Repository}}:{{.Tag}}" > $imagesFileName
+#docker image ls --format "{{.Repository}}:{{.Tag}}" > $imagesFileName
 
 images=$(cat ./$imagesFileName)
 
 ex="export IMAGE_"
 
 for image in $images
-do        
+do
+  imageName="$image"
+  if [[ $image =~ "/" ]]
+  then
   # 截取最后一个 / 之后的内容
   imageName=${image##*/}
+  fi
   # 截取 : 之前的内容
   name=${imageName%%:*}
   # - 替换为 _
   _name=${name//-/_}
   # 转大写
-  dxName=${_name^^} 
-  # 拼接
-  finalName=$ex$dxName"="$image 
+  dxName=${_name^^}
+  dxName=${dxName//\"}
 
- echo $finalName >> $imagesTmpFileName 
-  
+  # 拼接
+  finalName=$ex$dxName"="$image
+
+ echo $finalName >> $imagesTmpFileName
+
 done
 
 mv $imagesTmpFileName $imagesFileName
